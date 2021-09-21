a85b66fa8a9098c0db3186196fe2143598bfaf3d
ranit654 <83833310+ranit654@users.noreply.github.com>
2021-09-21 22:19:14 +0530

    DAY1_ASSIGNMENT

---
+++ DAY1_ASSIGNMENT.py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+Principle=float(input("Enter Principle"))
+Rate_Of_Interest=float(input("Enter Rate_Of_Interest"))
+Time=int(input("Enter Time"));
+SI=(Principle*Rate_Of_Interest*Time)/100
+print("Your simple interst is",SI)
